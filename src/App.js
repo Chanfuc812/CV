fc01a0da879bc0559bb0499758e134af1962681e
Chanfuc <nguoispecial@gmail.com>
2022-10-07 10:15:22 +0700

    update react app

--- src/App.js
+++ src/App.js
@@ -3,22 +3,86 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>
+          <img width="170" src="https://scontent.fsgn2-6.fna.fbcdn.net/v/t1.6435-9/193737753_3582409041984566_3021009673068075261_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=xfd2V1ni2EYAX-nHcWx&_nc_ht=scontent.fsgn2-6.fna&oh=00_AT_Ce0EUz67K7E6kQb7BpVqfRqHzvat_jcriZk0Pc8gWkw&oe=63519204"/>
+          <div>
+            DANG HAI TRANG PHUC
+          </div>
+        </h1>
+        <h2>
+          <div>
+            Web Developer
+
+          </div>
+        </h2>
+
+        <h3>
+
+          <div>
+            Date of birth: December 8, 2001
+          </div>
+          <div>
+            Gender: Male
+          </div>
+          <div>
+            Phone: 0926547935
+          </div>
+          <div>
+            Email: nguoispecial@gmail.com
+          </div>
+          <div>
+            Address: Ho Chi Minh city, Viet Nam
+          </div>
+          <div>
+            Website: https://facebook.com/chanfuccute
+          </div>
+
+        </h3>
+        <h4>
+
+        </h4>
+        <table border="1">
+          <tr>
+            <th>    EDUCATION   </th>
+            <th>    SKILLS       </th>
+            <th>    EXPERIENCE    </th>
+          </tr>
+          <tr>
+            <td>UIT(2019-2023)</td>
+            <td>Back-end</td>
+            <td>VNG Company (2023-2024)</td>
+          </tr>
+          <tr>
+            <td>CISA</td>
+            <td>Game programmer</td>
+            <td>Intel (2024-2026)</td>
+          </tr>
+          <tr>
+            <td>CISSP</td>
+            <td>Designer & Photoshop</td>
+            <td>Line (2026-2029)</td>
+          </tr>
+          <tr>
+            <td>IELTS 7.5</td>
+            <td>Content Creator</td>
+            <td>Streamer(Now)</td>
+          </tr>
+        </table>
+        <div>
+          <h5>
+            Anything else you want to ask me ❓
+          </h5>
+          <form>
+            <input type="text"placeholder="          Questions here"/>
+            <button>
+              Submit a question
+            </button>
+          </form>
+
+        </div>
+
+      </div>
   );
 }
 
